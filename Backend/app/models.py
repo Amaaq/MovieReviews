f53fe639b85c526105ab478c53eabc0034ba2aea
Adil Maaqoul <amaaqoul@sqli.com>
2024-04-05 09:21:11 +0000

    add models for reviews, actors and movies

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
-# Create your models here.
+class Actor(models.Model):
+    first_name= models.CharField(max_length=100)
+    last_name= models.CharField(max_length=100)
+    def __str__(self):
+        return self.first_name
+    
+    
+class Movie(models.Model):
+    title= models.CharField(max_length=100)
+    description= models.TextField()
+    actors= models.ManyToManyField(Actor)
+    def __str__(self):
+        return self.title
+    
+class Review(models.Model):
+    grade=models.PositiveSmallIntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
+    movie= models.ForeignKey(Movie,on_delete=models.CASCADE)
+    def __str__(self):
+        return f" {self.grade} for {self.movie.title}"
